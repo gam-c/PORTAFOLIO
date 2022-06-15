050be2ba2e4844ad7b31e070b3d8fafec2bbbae2
gam-c <stavish@gmail.com>
2022-06-15 17:03:45 -0400

    new

---
+++ index.js
@@ -0,0 +1,16 @@
+// template_ky7ejos
+// service_qdr8wxm
+// aA94QZ404ZE1_FY0g
+
+function contact(event) {
+    event.preventDefault()
+    emailjs
+        .sendForm(
+            'service_qdr8wxm',
+            'template_ky7ejos',
+            event.target,
+            'aA94QZ404ZE1_FY0g'
+        ).then(() => {
+            console.log('work')
+        })
+}
